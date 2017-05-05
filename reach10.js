2586314c85e251e9e07492b5949f0e88b1ce4ad6
Justin <chaos12135@users.noreply.github.com>
2017-05-05 14:23:49 -0700

    Update and rename reach2.js to reach10.js

--- reach10.js
+++ reach10.js
@@ -20,7 +20,8 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
-    document.getElementById("time1").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    $('#time1').html( days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+    //document.getElementById("time1").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     
     // If the count down is over, write some text 
     if (distance < 0) {
